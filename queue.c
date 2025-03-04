e3aea57943fd2fa6693ab5cad1c3ad7751d4dd4b
hwj222 <jackhuang.miat@gmail.com>
2025-03-04 22:01:12 +0800

    Add remove element function from head and tail
    Find the correct address of removed element and
    unlink their list heads.
    
    Check the string size and 'sp' pointer before copy.
    
    Change-Id: I35e6ea08b028385eef61558784d96f87f22e7a46

--- queue.c
+++ queue.c
@@ -79,13 +79,37 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *rm_elem = list_first_entry(head, element_t, list);
+    list_del(&rm_elem->list);
+
+    if (sp && bufsize > 0) {
+        strncpy(sp, rm_elem->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+
+    return rm_elem;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *rm_elem = list_last_entry(head, element_t, list);
+    list_del(&rm_elem->list);
+
+    if (sp && bufsize > 0) {
+        strncpy(sp, rm_elem->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+
+    return rm_elem;
 }
 
 /* Return number of elements in queue */

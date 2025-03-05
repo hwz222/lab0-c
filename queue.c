2d3bb8573bf59905ffcdb4d69983c92d272e8054
hwj222 <jackhuang.miat@gmail.com>
2025-03-05 10:51:32 +0800

    Add q_reverse function to reverse queue
    Traverse and exchange every node's previous and
    next pointers can reverse the order of the queue.
    
    Change-Id: I5e9abf3834e6ee2857a2a5422630f3556f44ff78

--- queue.c
+++ queue.c
@@ -177,7 +177,21 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+
+    struct list_head *ptr = head, *tmp;
+    do {
+        tmp = ptr->prev;
+        ptr->prev = ptr->next;
+        ptr->next = tmp;
+        ptr = ptr->prev;
+    } while (ptr != head);
+
+    return;
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
